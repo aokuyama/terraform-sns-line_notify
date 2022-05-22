4e90fbee83307b653d6048d02084378bbaad7ad9
aokuyama <ao980s@gmail.com>
2022-05-22 23:00:41 +0900

    add lambda skeleton;2C

--- variables.tf
+++ variables.tf
@@ -8,3 +8,11 @@ variable "region" {
   type    = string
   default = "ap-northeast-1"
 }
+variable "function_name" {
+  type    = string
+  default = "line_notify"
+}
+variable "line_to" {
+  type    = string
+  default = "xxxxxxxxxxxxxxxx"
+}
